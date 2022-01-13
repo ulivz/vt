051a44d81590d9fda858e15ad852edd69fc6e7af
chenhaoli <chenhaoli@bytedance.com>
2022-01-13 14:22:05 +0800

    chore: rename

--- packages/example/src/.vuepress/config.ts
+++ packages/example/src/.vuepress/config.ts
@@ -2,8 +2,8 @@ import { ThemeConfig } from "vuepress-theme-vuejs";
 import { defineConfig4CustomTheme } from "vuepress/config";
 
 export = defineConfig4CustomTheme<ThemeConfig>((ctx) => ({
-  theme: "vuejs",
-  title: "Vue.js",
+  theme: "vt",
+  title: "VT",
   themeConfig: {
     logo: 'https://vuejs.org/images/logo.svg',
     nav: [
