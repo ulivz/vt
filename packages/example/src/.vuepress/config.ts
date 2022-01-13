0373a306e3b8a9eb4caf08bd6f9292d6f797372a
chenhaoli <chenhaoli@bytedance.com>
2022-01-13 14:50:52 +0800

    docs: add logo

--- packages/example/src/.vuepress/config.ts
+++ packages/example/src/.vuepress/config.ts
@@ -6,7 +6,7 @@ export = defineConfig4CustomTheme<ThemeConfig>((ctx) => ({
   title: "VT",
   themeConfig: {
     repo: 'https://github.com/ulivz/vuepress-theme-vt',
-    logo: 'https://vuejs.org/images/logo.svg',
+    logo: '/logo.svg',
     nav: [
       { text: "Guide", link: "/guide/" },
       { text: "API", link: "/api/" },
