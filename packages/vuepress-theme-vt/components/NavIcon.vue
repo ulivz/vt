a6fab12e9af6a0fbbc344aa4d77c9c4f9dec333b
chenhaoli <chenhaoli@bytedance.com>
2023-02-26 04:30:27 +0800

    feat(theme): inheritable `NavIcon`

---
+++ packages/vuepress-theme-vt/components/NavIcon.vue
@@ -0,0 +1,58 @@
+<template>
+  <RouterLink :to="$localePath" class="home-link">
+    <img
+      v-if="$site.themeConfig.logo"
+      class="logo"
+      :src="$withBase($site.themeConfig.logo)"
+      :alt="$siteTitle"
+    />
+    <span
+      v-if="$siteTitle"
+      ref="siteName"
+      class="site-name"
+      :class="{ 'can-hide': $site.themeConfig.logo }"
+      >{{ $siteTitle }}</span
+    >
+  </RouterLink>
+</template>
+
+<style lang="stylus">
+.home-link {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  z-index: 1;
+  background-color: var(--vp-c-bg);
+  min-width: var(--vp-sidebar-width);
+  padding-left: calc(((100vw - var(--vp-screen-max-width)) / 2));
+  box-sizing: content-box;
+  background-color: var(--vp-c-bg-soft);
+  flex: 0;
+}
+
+.logo {
+  height: 1.4rem;
+  min-width: 1.4rem;
+  margin-right: 0.2rem;
+}
+
+.site-name {
+  font-size: 22px;
+  font-weight: 500;
+  color: var(--vp-c-text-1);
+  position: relative;
+}
+
+@media (max-width: $MQMobile) {
+  .site-name {
+    width: calc(100vw - 9.4rem);
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  .home-link {
+    background-color: transparent;
+  }
+}
+</style>
--- packages/vuepress-theme-vt/components/Navbar.vue
+++ packages/vuepress-theme-vt/components/Navbar.vue
@@ -2,23 +2,7 @@
   <header class="navbar">
     <div class="navbar-container">
       <SidebarButton @toggle-sidebar="$emit('toggle-sidebar')" />
-
-      <RouterLink :to="$localePath" class="home-link">
-        <img
-          v-if="$site.themeConfig.logo"
-          class="logo"
-          :src="$withBase($site.themeConfig.logo)"
-          :alt="$siteTitle"
-        />
-        <span
-          v-if="$siteTitle"
-          ref="siteName"
-          class="site-name"
-          :class="{ 'can-hide': $site.themeConfig.logo }"
-          >{{ $siteTitle }}</span
-        >
-      </RouterLink>
-
+      <NavIcon />
       <NavLinks class="can-hide" />
     </div>
   </header>
@@ -27,6 +11,7 @@
 <script>
 import SidebarButton from "@theme/components/SidebarButton.vue";
 import NavLinks from "@theme/components/NavLinks.vue";
+import NavIcon from "@theme/components/NavIcon.vue";
 
 export default {
   name: "Navbar",
@@ -34,6 +19,7 @@ export default {
   components: {
     SidebarButton,
     NavLinks,
+    NavIcon,
   },
 };
 </script>
@@ -57,35 +43,6 @@ export default {
     padding-right: calc(((100vw - var(--vp-screen-max-width)) / 2));
   }
 
-  .home-link {
-    height: 100%;
-    display: flex;
-    align-items: center;
-    z-index: 1;
-    background-color: var(--vp-c-bg);
-    min-width: var(--vp-sidebar-width);
-    padding-left: calc(((100vw - var(--vp-screen-max-width)) / 2));
-    box-sizing: content-box;
-    background-color: var(--vp-c-bg-soft);
-    flex: 0;
-  }
-
-  a, span, img {
-    display: inline-block;
-  }
-
-  .logo {
-    height: 1.4rem;
-    min-width: 1.4rem;
-    margin-right: 0.2rem;
-  }
-
-  .site-name {
-    font-size: 22px;
-    font-weight: 500;
-    color: var(--vp-c-text-1);
-    position: relative;
-  }
 }
 
 @media (max-width: $MQMobile) {
@@ -96,17 +53,6 @@ export default {
 
     .can-hide {
       display: none;
-    }
-
-    .site-name {
-      width: calc(100vw - 9.4rem);
-      overflow: hidden;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-    }
-
-    .home-link {
-      background-color: transparent;
     }
   }
 }
